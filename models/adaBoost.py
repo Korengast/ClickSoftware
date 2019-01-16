95358f8966ca9db570977f4a889897d4a84bcccf
Korengast <32592798+Korengast@users.noreply.github.com>
2019-01-16 15:43:56 +0200

    Push to git

---
+++ models/adaBoost.py
@@ -0,0 +1,10 @@
+__author__ = "Koren Gast"
+from models.model import GenModel
+from sklearn.ensemble import AdaBoostClassifier
+
+
+class AdaBoost(GenModel):
+    def __init__(self, n_estimators=50):
+        super().__init__()
+        self.model = AdaBoostClassifier(n_estimators=n_estimators)
+        self.name = "Random forest"
